573dbf500f1ec9d7d01a4c7f0074813d83a116c6
bashangbasir <bassam.abd.basir@gmail.com>
2021-02-27 15:49:45 +0800

    Update test_setel_assignment.py
    Create test case 2.
    - “Create Task via mobile phone”
    
    Update test case 1:
    - use the save response and load response to json file

--- tests/test_setel_assignment.py
+++ tests/test_setel_assignment.py
@@ -1,11 +1,11 @@
- """
+"""
 TEST CASE 1 - “Create Project”
 1.Create test project via API.
 2.Login into mobile application.
 3.Verify on mobile that project is created
 
 TEST CASE 2 - “Create Task via mobile phone”
-1.Create test task via mobile application in test project.
+1. Create test task via mobile application in test project.
 2. API: Verify that task created correctly.
 
 TEST CASE 3 - “Reopen Task”
@@ -21,18 +21,14 @@ from pages.welcome_page import WelcomePage
 from pages.login_page import LoginPage
 from pages.user_home_page import UserHomePage
 from api.todoist_api import TodoistAPI
-
-test_task_id = ""
-task_name = "Create Task 1"
-project_id = ""
-project_name = "Test Project 1"
+import time
 
 def test_create_project(driver):
 
     #1.Create test project via API.
-    api = TodoistAPI()
-    r = api.create_project(project_name)
-    project_id = r.json()["id"]
+    todoist_api = TodoistAPI()
+    r = todoist_api.create_project("TEST PROJECT")
+    todoist_api.save_response(r,"project.json")
     assert (r.status_code == 200), "Project not created."
 
     #2.Login into mobile application.
@@ -42,18 +38,46 @@ def test_create_project(driver):
     login_page = LoginPage(driver)
     login_page.login()
     user_home_page = UserHomePage(driver)
-    assert "Today" in user_home_page.get_toolbar_title() , "Wrong toolbar title"
+    assert "Today" in user_home_page.get_toolbar_title("Today") , "Wrong toolbar title"
 
     #Verify on mobile that project is created
     all_projects = user_home_page.get_all_projects()
-    assert project_name in all_projects , "The created project is not in the mobile app" 
-    print(project_id)
-    raise Exception("Test Incomplete!")
+    assert todoist_api.load_save_data("project.json")["name"] in all_projects , "The created project is not in the mobile app" 
+    
+    #raise Exception("Test Incomplete!")
 
 def test_create_task_via_mobile_app(driver):
-   #TODO
-   pass
+    todoist_api = TodoistAPI()
+    #read the saved reponse previous test
+    data = todoist_api.load_save_data("project.json")
+    project_name = data["name"]
+    project_id = data["id"]
+    #1. Create test task via mobile application in test project.
+    welcome_page = WelcomePage(driver)
+    assert "Welcome to Todoist" in welcome_page.get_welcome_message(),"Wrong Welcome title"
+    welcome_page.start_login_with_email()
+    login_page = LoginPage(driver)
+    login_page.login()
+    user_home_page = UserHomePage(driver)
+    assert "Today" in user_home_page.get_toolbar_title("Today") , "Wrong toolbar title"
+    user_home_page.go_to_project(project_name)
+    assert project_name in user_home_page.get_toolbar_title(project_name), "Wrong project"
+    task_name = "TASK CREATE"
+    user_home_page.create_task(task_name)
+    print(project_id)
+   
+    #2. API: Verify that task created correctly.
+    time.sleep(3)
+    r = todoist_api.get_active_tasks(project_id)
+    #save response
+    todoist_api.save_response(r,"task.json")
+    assert r.status_code == 200 , "not able to get active task"
+    assert r.json()[0]["content"] == task_name , "Task is not created"
+    
+    #raise Exception("Test Incomplete!")
+    
 
 def test_reopen_task(driver):
     #TODO
+    raise Exception("Test Incomplete!")
     pass
