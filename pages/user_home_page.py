c2e5052395ebc18d6a82dcbe408deae1602b63a3
bashangbasir <bassam.abd.basir@gmail.com>
2021-02-27 15:48:33 +0800

    Update user_home_page.py
    Add method
    - to click on project
    - create task
    
    Update method :
    - update method get toolbar

--- pages/user_home_page.py
+++ pages/user_home_page.py
@@ -4,20 +4,26 @@ This module contain the page object for the user home page.
 
 from appium.webdriver.common.mobileby import MobileBy 
 import time
+
 class UserHomePage:
     
-    TOOLBAR_TITLE = (MobileBy.XPATH,"*//android.widget.TextView[@text='Today']")
     BURGER_NAV = (MobileBy.ACCESSIBILITY_ID,"Change the current view")
     ALL_PROJECT_LISTS = (MobileBy.XPATH,"*//android.widget.RelativeLayout[@resource-id='android:id/content']")
     PROJECT_TEXT = (MobileBy.XPATH,"//android.widget.TextView[@resource-id='com.todoist:id/name']")
     PROJECTS_BTN = (MobileBy.XPATH,"*//android.widget.RelativeLayout/android.widget.TextView[@text='Projects']")
+    CREATE_TASK_BTN = (MobileBy.ID,"com.todoist:id/fab")
+    TASK_CONTENT_TEXTBOX = (MobileBy.ID,"android:id/message")
+    ADD_TASK_BTN = (MobileBy.ACCESSIBILITY_ID,"Add")
+    TASKS_LISTS = (MobileBy.ID,"com.todoist:id/root")
+    TASK_TEXT = (MobileBy.XPATH)
 
 
     def __init__(self,driver):
         self.driver = driver
 
-    def get_toolbar_title(self):
-        return self.driver.find_element(*self.TOOLBAR_TITLE).text
+    def get_toolbar_title(self,toolbar):
+        xpath = "*//android.view.ViewGroup[@resource-id='com.todoist:id/toolbar']/android.widget.TextView[@text='{}']".format(toolbar)
+        return self.driver.find_element(MobileBy.XPATH,xpath).text
 
     def get_all_projects(self):
         all_projects = []
@@ -28,4 +34,28 @@ class UserHomePage:
             #append all the project name seen from mobile app 
             all_projects.append(elem.find_element(*self.PROJECT_TEXT).text)
         return all_projects
+
+    def go_to_project(self,project_name):
+        xpath_project = "*//android.widget.RelativeLayout/android.widget.TextView[@text='{}'".format(project_name)
+        self.driver.find_element(*self.BURGER_NAV).click()
+        self.driver.find_element(*self.PROJECTS_BTN).click()
+        project_elems = self.driver.find_elements(*self.ALL_PROJECT_LISTS)
+        for elem in project_elems:
+            if project_name == elem.find_element(*self.PROJECT_TEXT).text:
+                elem.find_element(*self.PROJECT_TEXT).click()
+
+        
+
+    def create_task(self,task_name):
+        self.driver.find_element(*self.CREATE_TASK_BTN).click()
+        self.driver.find_element(*self.TASK_CONTENT_TEXTBOX).send_keys(task_name)
+        self.driver.find_element(*self.ADD_TASK_BTN).click()
+        self.driver.find_element(*self.ADD_TASK_BTN).click()
+        
+        #self.driver.press_keycode(4)
+
+
+
+
+
 
